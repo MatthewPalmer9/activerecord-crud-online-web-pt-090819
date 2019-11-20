95973bcce1c1e65cae7b3fc74ff2b81d3f927317
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2019-11-20 03:44:35 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -6,7 +6,7 @@
 # end                              # end
 
 def can_be_instantiated_and_then_saved
-  movie = Movie.new()
+  movie = Movie.new(title: "The Sting")
   movie.title = "This is a title."
   __
 end
